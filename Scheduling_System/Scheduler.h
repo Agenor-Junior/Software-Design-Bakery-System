649d825a4e68632c2a5999ae6479d150738cf539
Agenor Junior <66803750+Agenor-Junior@users.noreply.github.com>
2025-01-07 21:58:26 -0500

    Update Scheduler.h

--- Scheduling_System/Scheduler.h
+++ Scheduling_System/Scheduler.h
@@ -12,7 +12,7 @@ private:
     std::vector<Order> orders; // Vector to store orders 
 
 public:
-	void addOrder(const Order& order); // Method to add an order to the scheduler 
+	void addOrder(const Order& order); // Method to add an order to the scheduler.
     std::vector<Order> queryOrdersByDate(const std::string& date) const; // Method to query orders by date 
     void convertOrderToInvoice(const std::string& orderID); // Method to convert an order to an invoice 
 };
